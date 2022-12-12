48acedb80de0fcf901f712f80b61f7867279fbd8
Jay Welsh <thealchemicalopus@gmail.com>
2022-12-12 17:58:10 +0200

    feat(transformers/output): indentation

--- src/database/transformers/rate/output.ts
+++ src/database/transformers/rate/output.ts
@@ -4,26 +4,26 @@ import { IRate } from "../../../interfaces";
 
 class RateTransformer extends BaseTransformer {
   transform(rateEntry: IRate) {
-      return {
-        rate: rateEntry.rate,
-        side: rateEntry.side,
-        timestamp: rateEntry.timestamp,
-        ...(rateEntry.silo && {
-          silo: {
-            name: rateEntry.silo.name,
-            address: rateEntry.silo.address,
-            input_token_address: rateEntry.silo.input_token_address
-          }
-        }),
-        ...(rateEntry.asset && {
-          asset: {
-            address: rateEntry.asset.address,
-            symbol: rateEntry.asset.symbol,
-            decimals: rateEntry.asset.decimals,
-          }
-        }),
-        type: rateEntry.type,
-      }
+    return {
+      rate: rateEntry.rate,
+      side: rateEntry.side,
+      timestamp: rateEntry.timestamp,
+      ...(rateEntry.silo && {
+        silo: {
+          name: rateEntry.silo.name,
+          address: rateEntry.silo.address,
+          input_token_address: rateEntry.silo.input_token_address
+        }
+      }),
+      ...(rateEntry.asset && {
+        asset: {
+          address: rateEntry.asset.address,
+          symbol: rateEntry.asset.symbol,
+          decimals: rateEntry.asset.decimals,
+        }
+      }),
+      type: rateEntry.type,
+    }
   }
 }
 
--- src/database/transformers/tvlTotal/output.ts
+++ src/database/transformers/tvlTotal/output.ts
@@ -4,25 +4,25 @@ import { ITvlTotal } from "../../../interfaces";
 
 class TvlTotalOutputTransformer extends BaseTransformer {
   transform(tvlTotalEntry: ITvlTotal) {
-      return {
-        tvl: tvlTotalEntry.tvl,
-        timestamp: tvlTotalEntry.timestamp,
-        meta: tvlTotalEntry.meta,
-        ...(tvlTotalEntry.silo && { 
-          silo:{
-            name: tvlTotalEntry.silo.name,
-            address: tvlTotalEntry.silo.address,
-            input_token_address: tvlTotalEntry.silo.input_token_address
-          }
-        }),
-        ...(tvlTotalEntry.asset && {
-          asset: {
-            address: tvlTotalEntry.asset.address,
-            symbol: tvlTotalEntry.asset.symbol,
-            decimals: tvlTotalEntry.asset.decimals,
-          }
-        }),
-      }
+    return {
+      tvl: tvlTotalEntry.tvl,
+      timestamp: tvlTotalEntry.timestamp,
+      meta: tvlTotalEntry.meta,
+      ...(tvlTotalEntry.silo && { 
+        silo:{
+          name: tvlTotalEntry.silo.name,
+          address: tvlTotalEntry.silo.address,
+          input_token_address: tvlTotalEntry.silo.input_token_address
+        }
+      }),
+      ...(tvlTotalEntry.asset && {
+        asset: {
+          address: tvlTotalEntry.asset.address,
+          symbol: tvlTotalEntry.asset.symbol,
+          decimals: tvlTotalEntry.asset.decimals,
+        }
+      }),
+    }
   }
 }
 
