ca539cd6ea8f26fe064ebbd4ffd9c11614b51590
Jay Welsh <thealchemicalopus@gmail.com>
2022-12-13 20:41:50 +0200

    chore(subgraph): migrate from 2.1.0 -> 2.0.3

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,5 @@
-export const SUBGRAPH_ENDPOINT = "https://api.thegraph.com/subgraphs/id/QmcsWW8j9jKvCbfv4CfoavYBbWMmHfdgHyXMj3Pm9s9ihA";
-export const SUBGRAPH_VERSION = "2.1.0";
+export const SUBGRAPH_ENDPOINT = "https://api.thegraph.com/subgraphs/id/QmT8631Rt8ZaGtD48F7WP6trs5Wd2RNoacMRPYLfuotDwB";
+export const SUBGRAPH_VERSION = "2.0.3";
+export const SUBGRAPH_ENDPOINT_FALLBACK = "https://api.thegraph.com/subgraphs/id/QmQV7RB3WUFuSrCSyJsDq6FkQsVYFwak2xCZYsq3JmDrSq";
+export const SUBGRAPH_VERSION_FALLBACK = "2.0.2";
 export const MAX_MINUTELY_RATE_ENTRIES = 1441;
