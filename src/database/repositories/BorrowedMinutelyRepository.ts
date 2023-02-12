2376614a829ba774216454a8b24a70c2481f6e56
Jay Welsh <thealchemicalopus@gmail.com>
2023-02-12 19:08:38 +0200

    feat(commands): adds tvl command to Discord Bot

--- src/database/repositories/BorrowedMinutelyRepository.ts
+++ src/database/repositories/BorrowedMinutelyRepository.ts
@@ -1,9 +1,20 @@
+import { QueryBuilder } from "objection";
+
 import { BorrowedMinutelyModel } from "../models";
 import BorrowedBaseRepository from "./BorrowedBaseRepository";
 
 class BorrowedMinutelyRepository extends BorrowedBaseRepository {
   getModel() {
     return BorrowedMinutelyModel
+  }
+
+  async getLatestResultByNetworkAndMeta(network: string, meta: string) {
+    const result = await BorrowedMinutelyModel.query().where(function (this: QueryBuilder<BorrowedMinutelyModel>) {
+      this.where('network', network);
+      this.where('meta', meta);
+    }).orderBy('timestamp', 'DESC').first();
+
+    return this.parserResult(result);
   }
 }
 
