e2ce5c82dccbe940291b0df9a1e30d85a762c0f6
Jay Welsh <thealchemicalopus@gmail.com>
2023-11-12 11:15:52 +0200

    feat(EventController): enable queries of ARB reward claims

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -5,6 +5,7 @@ import { body } from 'express-validator';
 import Router from "./Router";
 
 Router.get('/events', [], 'EventController@getEventsWholePlatform');
+Router.get('/events/rewards', [], 'EventController@getRewardEvents');
 Router.get('/events/:eventType', [], 'EventController@getEventsWholePlatform');
 Router.get('/events/:eventType/distinct-daily-users', [], 'EventController@getEventsDistinctUsersPerDayWholePlatform');
 Router.get('/events/:eventType/:siloAddressOrName', [], 'EventController@getEventsBySilo');
