136acb1f73f90dff80031cd595f3d336171094f2
Jay Welsh <thealchemicalopus@gmail.com>
2025-10-02 23:31:49 +0200

    chore(constants): use WBTC price proxy for xBTC

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -680,6 +680,9 @@ export const PRICE_PROXIES : {[key: string]: {[key: string]: {proxyAddress: stri
   avalanche: {
     "0x649342c6bff544d82DF1B2bA3C93e0C22cDeBa84": { // savBTC
       proxyAddress: "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c", // Avalanche Bridged WBTC
+    },
+    "0x6eAf19b2FC24552925dB245F9Ff613157a7dbb4C": { // xBTC
+      proxyAddress: "0x0555E30da8f98308EdB960aa94C0Db47230d2B9c", // Avalanche Bridged WBTC
     }
   }
 }
